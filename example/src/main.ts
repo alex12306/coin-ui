e320103612dfbe17606d4e3ba0367f548c1f1f0a
alex12306 <alexcoder666@outlook.com>
2023-09-04 16:52:48 +0800

    基础结构完成

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 //用于测试打包
-import coin from 'coin-ui';
-import 'coin-ui/style';
+// import coin from 'coin-ui';
+// import 'coin-ui/style';
 //用于开发调试
-// import coin from '../../packages/coin-ui/index';
+import coin from '../../packages/coin-ui/index';
 
 const app = createApp(App);
 app.use(coin);
