6c4a8ebcc51f5e87907d28af01e67ad3b8516d01
alex12306 <alexcoder666@outlook.com>
2023-09-05 18:01:14 +0800

    fix: test commitlint type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+// eslint-disable-next-line
+module.exports = {
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'release',
+        'style',
+        'test',
+        'improvement'
+      ]
+    ]
+  },
+  extends: ['@commitlint/config-conventional']
+}
